82dfcfb356212d5bf15963574255d5ec9d14ff6c
ebirch <ethanjbirch@gmail.com>
2014-03-27 13:47:50 -0600

    Both D&C algorithms now working

--- closestPair.rb
+++ closestPair.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/ruby
 
-# points = Array.new(ARGV.first.to_i){[rand(100), rand(100)]}
-# points = Array.new(16){[rand(100), rand(100)]}
-points = [[46, 91], [85, 1], [2, 95], [52, 27], [80, 94], [23, 9], [6, 20], [94, 33], [47, 57], [50, 51], [49, 54], [42, 75], [8, 39], [42, 75], [86, 52], [51, 9]]
+points = Array.new(ARGV.first.to_i){[rand(100), rand(100)]}
 
 def dist(p1, p2)
+	return Float::INFINITY if p1.nil? || p2.nil?
 	Math.sqrt((p1.first - p2.first) ** 2 + (p1.last - p2.last) ** 2)
 end
 
@@ -13,24 +12,24 @@ def dumbAlgo(points)
 end
 
 def slowDC(points)
-	puts points.inspect
 	return dist(points.first, points.last) if points.size == 2
 	left, right = points.each_slice(points.size / 2).to_a
 	min = [slowDC(left), slowDC(right)].min
 	mid = points[points.size / 2].first
-	puts "#{mid}, #{min}"
-	# puts points.select{|x| (mid - min..mid + min) === x.first}.inspect, ''
-	[min, slowDC(points.select{|x| (mid - min..mid + min) === x.first})].min
+	[min, dumbAlgo(points.select{|x| (mid - min..mid + min) === x.first})].min
 end
 
 def fastDC(points)
-
+	return dist(points.first, points.last) if points.size == 2
+	left, right = points.each_slice(points.size / 2).to_a
+	min = [fastDC(left), fastDC(right)].min
+	mid = points[points.size / 2].first
+	strip = points.select{|x| (mid - min..mid + min)}.sort{|x, y| x.last <=> y.last}
+	[min, (0..strip.size).map{|x| (1..6).map{|y| dist(strip[x], strip[x + y])}.min}.min].min
 end
 
-# puts points.inspect
-
-# puts dumbAlgo(points)
+puts dumbAlgo(points)
 
 points.sort!{|x, y| x.first <=> y.first}
 puts slowDC(points)
-# puts points.inspect
+puts fastDC(points)
